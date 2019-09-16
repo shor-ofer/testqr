c8a30c802f189a0638305e04691a6e97eb5a0f44
plpl <shor.ofer@live.com>
2019-09-16 22:08:43 +0300

    c1

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,19 @@
 import * as React from "react";
 import { render } from "react-dom";
-var QRCode = require('qrcode.react');
+var QRCode = require("qrcode.react");
 
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-    <QRCode value="https://shorofer.wixsite.com/xmmos" />
+      <div className="qrtest">
+        <QRCode
+          size="80"
+          includeMargin="true"
+          value="https://shorofer.wixsite.com/xmmos"
+        />
+      </div>
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
